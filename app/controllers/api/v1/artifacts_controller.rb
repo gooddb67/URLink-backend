3c1df5543be3af29d800b8b66881b652be317808
Dave Goodman <davegoodman20@gmail.com>
2017-11-23 14:16:52 -0500

    controller changes and routes

--- app/controllers/api/v1/artifacts_controller.rb
+++ app/controllers/api/v1/artifacts_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::ArtifactsController < ApplicationController
+
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @subtopic = Subtopic.find(params[:subtopic_id])
+
+    @artifacts = Artifact.where(topic_id: @topic.id, subtopic_id: @subtopic.id)
+    render json: @artifacts
+  end
+
+  def create
+    @topic = Topic.find(params[:id])
+    @subtopic = Subtopic.find(params[:id])
+    @artifact = Artifact.create(media: params[:media], url: params[:url], topic_id: @topic.id, subtopic_id: @subtopic_id)
+
+    if @artifact.save
+      render json: @artifact
+    else
+      render json {@artifact.errors.full_messages}
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :topics, only: [:index, :create] do
-        resources :subtopics, only: [:index, :create, :show, :update]
+        resources :subtopics, only: [:index, :create, :show, :update] do
+          resources :artifacts, only: [:index, :create, :update] do
+            resources :comments, only: [:index, :create]
+          end
+        end
       end
     end
   end
