11179a92458bd76625348c472479f1a88091b5cb
Dave Goodman <davegoodman20@gmail.com>
2018-01-03 10:47:58 -0500

    added caption to artifact

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   has_many :user_topics
   has_many :users, through: :user_topics
-  has_many :subtopics
+  has_many :subtopics, dependent: :destroy
   has_many :artifacts, through: :subtopics
 end
---
+++ db/migrate/20180103154425_add_caption_to_artifact.rb
@@ -0,0 +1,5 @@
+class AddCaptionToArtifact < ActiveRecord::Migration[5.1]
+  def change
+    add_column :artifacts, :caption, :string 
+  end
+end
